5da15ee11e34f0a1d09d281d7775fdd942a7a954
milosavljevic92 <milosavljevic.damir@gmail.com>
2024-08-23 04:25:11 +0200

    adjust for esp32

--- Firmware/bmw-pwm-ligts/bmw-pwm-ligts.ino
+++ Firmware/bmw-pwm-ligts/bmw-pwm-ligts.ino
@@ -1,63 +1,60 @@
 /* bmw-pwm-lights: Main firmware
  - WHAT IT DOES
  - SEE the comments after "//" on each line below
- - CONNECTIONS: Arduino Nano V3
+ - CONNECTIONS: ESP32
    - 
    - 
  - V1.00 08/22/2024
    Questions: */
 
 // define ports
-#define PWM_OUTPUT_1 5
-#define PWM_OUTPUT_2 6
-#define INPUT_POSITION 7
-#define INPUT_BRAKE 8
+#define PWM_OUTPUT_1 12
+#define PWM_OUTPUT_2 14
+#define INPUT_POSITION 35
+#define INPUT_BRAKE 34
 
 // define const
 int BRAKE_INTESITY = 100; // from 0 - 100 
-int POSITION_INTESITY = 80; // from 0 - 100 
-int SCALE_STABIL_BLOCK = 1;
-int SCALE_WEIGHT_BLOCK = 2;
+int POSITION_INTESITY = 50; // from 0 - 100 
+
 int DEBUG_SERIAL_BAUNDRATE = 9600;
 
-int lastBrakeState = LOW;
-int lastPositionState = LOW;
 int pwmValue = 0;
+int LIGHT_POSITION;
+int LIGHT_BRAKE;
 
 void setup() {
   pinMode(INPUT_BRAKE, INPUT);
   pinMode(INPUT_POSITION, INPUT);
   pinMode(PWM_OUTPUT_1, OUTPUT);
-  
+  analogWrite(PWM_OUTPUT_1, pwmValue);
   Serial.begin(DEBUG_SERIAL_BAUNDRATE);
 }
 
 void loop() {
-  int currentBrakeState = digitalRead(INPUT_BRAKE);
-  int currentPositionState = digitalRead(INPUT_POSITION);
-  
-  if (currentBrakeState != lastBrakeState) {
-    pwmValue = map(BRAKE_INTESITY, 0, 100, 0, 255); // Mapa vrednosti 0-100 na PWM raspon 0-255
-    analogWrite(PWM_OUTPUT_1, pwmValue);
+  LIGHT_POSITION = digitalRead(INPUT_POSITION);
+  LIGHT_BRAKE = digitalRead(INPUT_BRAKE);
 
-    Serial.print("Brake state changed to: ");
-    Serial.println(currentBrakeState);
-    Serial.print("Setting PWM to: ");
-    Serial.println(pwmValue);
-    
-    lastBrakeState = currentBrakeState;
-    loop;
-  }
-  if (currentPositionState != lastPositionState) {
+  if(LIGHT_POSITION == 1 && LIGHT_BRAKE == 0) {
     pwmValue = map(POSITION_INTESITY, 0, 100, 0, 255);
     analogWrite(PWM_OUTPUT_1, pwmValue);
-    
-    Serial.print("Position state changed to: ");
-    Serial.println(currentPositionState);
-    Serial.print("Setting PWM to: ");
-    Serial.println(pwmValue);
-
-    lastPositionState = currentPositionState;
   }
-  delay(50);
+
+  if(LIGHT_POSITION == 0 && LIGHT_BRAKE == 0) {
+    analogWrite(PWM_OUTPUT_1, 0);
+  }
+
+  if(LIGHT_BRAKE == 1) {
+    pwmValue = map(BRAKE_INTESITY, 0, 100, 0, 255);
+    analogWrite(PWM_OUTPUT_1, pwmValue);
+  }
+
+  Serial.print("POSITION");
+  Serial.print(" : ");
+  Serial.print(LIGHT_POSITION);
+  Serial.print("  ");
+  Serial.print("BRAKE");
+  Serial.print(" : ");
+  Serial.println(LIGHT_BRAKE);
+  delay(100);
 }
